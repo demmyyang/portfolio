e08837211fcb88eea952a51c9bfd929f604de4ff
Demmy Yang <77114679+demmyyang@users.noreply.github.com>
2021-07-31 04:10:55 -0400

    preload 100

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const preloader = document.querySelector('.preloader');
 
 const fadeEffect = setInterval(() => {
-    // if we don't set opacity 1 in CSS, then
-    // it will be equaled to "" -- that's why
-    // we check it, and if so, set opacity to 1
     if (!preloader.style.opacity) {
     preloader.style.opacity = 1;
     }
