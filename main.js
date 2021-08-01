13aaee81709f61a6d545aae6eebad7da75efe557
Demmy Yang <77114679+demmyyang@users.noreply.github.com>
2021-07-31 20:16:21 -0400

    responsive nav with animation

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',() => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
+
+
 const preloader = document.querySelector('.preloader');
 
 const fadeEffect = setInterval(() => {
@@ -12,4 +37,3 @@ const fadeEffect = setInterval(() => {
 }, 100);
 
 window.addEventListener('load', fadeEffect);
-
