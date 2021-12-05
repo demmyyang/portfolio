e81021afe528e99c917320ec8328b1679e8706f6
Demmy Yang <77114679+demmyyang@users.noreply.github.com>
2021-12-05 13:02:27 -0500

    lots of css and js changes

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-container');
-    const navLinks = document.querySelectorAll('.nav-container li');
-
-    burger.addEventListener('click',() => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
-}
-
-navSlide();
-
-
 const preloader = document.querySelector('.preloader');
 
 function fadeEffect() {
@@ -46,9 +22,10 @@ function fadeEffect() {
 // Immediatly hide preloader for mobile browsers
 const hasPreloaded = window.sessionStorage.getItem('shownPreload');
 if (document.body.scrollWidth < 800 || hasPreloaded !== null) {
-    preloader.style.opacity = 0;
+    if (preloader) {
+        preloader.style.opacity = 0;
+    }
 } else {
     window.sessionStorage.setItem('shownPreload', true);
     window.addEventListener('load', fadeEffect);
 }
-
